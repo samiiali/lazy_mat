7da51fa24151872e8a9acd5a5b3558cdb40c57fc
Ali Samii <a.samii@gmail.com>
2020-10-16 00:34:48 -0500

    add vec tests

--- test/tests.cpp
+++ test/tests.cpp
@@ -29,6 +29,10 @@ TEST (test_suite, storage_test)
     ASSERT_EQ(stor1.index({0,0,0}), 0);
     ASSERT_EQ(stor2.index({3,2,4}), 4*3*5-1);
     ASSERT_DEATH(stor2.index({3,3,4}), ".*");
+    linalg::mat_stor_t stor9(linalg::stor_fmt_t::col_maj);
+    stor9.resize(2,2);
+    stor9(0,1) = 1.2;
+    EXPECT_EQ(stor9.data_at(2), 1.2);
 }
 
 TEST (test_suite, empty_ctor)
@@ -85,6 +89,7 @@ TEST (test_suite, move_with_different_dim)
 TEST (test_suite, output)
 {
     linalg::mat_t mat1(2,2);
+    mat1.assign(0.0);
     mat1(0,1) = 1.0;
     std::ostringstream output;
     output << mat1;
@@ -142,8 +147,49 @@ TEST (test_suite, sub_test)
     EXPECT_EQ(mat3(1,0), -1.0);
 }
 
-TEST (test_suite, vec_test)
+TEST (test_suite, vec_print)
 {
-    linalg::vec_t vec1(5);
-    std::cout << vec1 << "\n";
+    linalg::vec_t vec1(3);
+    vec1.assign(0.0);
+    vec1(1) = 1.0;
+    std::ostringstream oss;
+    oss << vec1;
+    EXPECT_EQ(oss.str(), "  0.0000e+00\n  1.0000e+00\n  0.0000e+00\n");
+}
+
+TEST (test_suite, vec_operation)
+{
+    linalg::vec_t vec1(3);
+    linalg::vec_t vec3(2);
+    linalg::vec_t vec4(3);
+    vec1.assign(1.0);
+    linalg::mat_t mat1(2,3);
+    mat1.assign(2.0);
+    linalg::vec_t vec2;
+    std::ostringstream oss;
+    oss << vec2;
+    EXPECT_EQ(oss.str(), "\n");
+    oss.str("");
+    vec2 = mat1 * vec1;
+    vec3 = mat1 * vec1;
+    EXPECT_DEATH({vec4=mat1*vec1;}, ".*");
+    oss << vec2;
+    EXPECT_EQ(oss.str(), "  6.0000e+00\n  6.0000e+00\n");
+}
+
+TEST (test_suite, vec_op2)
+{
+    linalg::vec_t vec1(3, linalg::vec_type_t::row_vec);
+    vec1.assign(2.0);
+    vec1(1) = 3.5;
+    EXPECT_EQ(vec1.data_at(1), 3.5);
+    EXPECT_EQ(vec1.data()[1], 3.5);
+    vec1.resize(5);
+    EXPECT_EQ(vec1._type, linalg::vec_type_t::row_vec);
+    vec1.resize(6, linalg::vec_type_t::col_vec);
+    vec1.resize(6, linalg::vec_type_t::row_vec);
+    linalg::mat_t mat1(6,6);
+    mat1.assign(1.5);
+    linalg::vec_t vec2 = vec1 * mat1;
+    linalg::mat_t mat3(1,6);
 }
